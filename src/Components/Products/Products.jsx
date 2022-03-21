e1001ed7d5f11a73ba8106b4d9a7b951c80f78e4
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-03-21 20:12:53 +0600

    Utilities shared function - used add and multiply function

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -1,15 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import Product from '../Product/Product';
+import { add, multiply } from '../Utilities/sum';
 
 const Products = () => {
     // ---------------- One Way ----------------
-/*     const products = [
+    const allProducts = [
         {id:1, name:'Desktop', price: 70000},
         {id:2, name:'Laptop', price: 35000},
         {id:3, name:'Phone', price: 15000},
         {id:4, name:'UPS', price: 3000},
         {id:5, name:'Speaker', price: 700},
-    ] */
+    ]
 
     // ---------------- Another Way ----------------
     const [products, setProducts] = useState([])
@@ -24,6 +25,8 @@ const Products = () => {
             {
                 products.map(product => <Product product={product}></Product>)
             }
+            <h3><strong>Add Result:</strong> { add(+allProducts[0].price , allProducts[1].price)}</h3>
+            <h3><strong>Multiply Result:</strong> { multiply(+allProducts[3].price , allProducts[4].price)}</h3>
         </div>
     );
 };
