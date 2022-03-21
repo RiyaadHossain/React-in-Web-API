eaa033fee947fce884524384a7269878f8f90ce0
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-03-21 20:02:56 +0600

    Another way of showing data

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Product from '../Product/Product';
 
 const Products = () => {
-    const products = [
+    // ---------------- One Way ----------------
+/*     const products = [
         {id:1, name:'Desktop', price: 70000},
         {id:2, name:'Laptop', price: 35000},
         {id:3, name:'Phone', price: 15000},
         {id:4, name:'UPS', price: 3000},
         {id:5, name:'Speaker', price: 700},
-    ]
+    ] */
+
+    // ---------------- Another Way ----------------
+    const [products, setProducts] = useState([])
+    useEffect(() => {
+        fetch('data.json')
+        .then(res => res.json())
+        .then(data => setProducts(data))
+    },[])
     return (
         <div>
             <h1>⌨️ Electronic Products 💻</h1>
